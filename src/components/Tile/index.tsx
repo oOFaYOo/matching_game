a69b5f85f4cdde3c563a49e5a3cd2dc5dc9fbb83
Nika Ts <nika.tseleva@mail.ru>
2023-06-17 01:29:29 +0400

    try add animation

--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -11,13 +11,21 @@ const Tile = ({image, title, open, index, clickHandler} : ITile) => {
         <div className={`${theme === 'dark' 
             ? 'hover:shadow-lg border-transparent hover:bg-neutral-600 bg-neutral-600/70 hover:shadow-black hover:border-amber-500' 
             : 'border-neutral-300 hover:border-amber-500 hover:shadow-lg hover:bg-white'} 
-        relative rounded-2xl flex justify-center items-center hover:cursor-pointer m-1 border-2 border-dashed w-24 h-24`}
+        relative rounded-2xl flex justify-center items-center hover:cursor-pointer m-1 border-2 border-dashed w-24 h-24 duration-1000
+        ${open ? '' : 'card_open'}`}
         onClick={() => clickHandler(index, title)}
+
         >
             {
                 open
-                    ? <img src={image} className={'w-20 h-20'} />
-                    : null
+                    ? <img src={image} className={'w-20 h-20'}
+                           style={{
+                               backfaceVisibility: 'hidden',
+                           }}/>
+                    : <div className={'relative w-full h-full'}
+                           style={{
+                               backfaceVisibility: 'hidden',
+                           }}></div>
             }
         </div>
     )
